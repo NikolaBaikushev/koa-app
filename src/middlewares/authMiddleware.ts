6d0fdbca320eea7b06ecc6757d5f597733a7ad20
NikolaBaikushev <234618369+NikolaBaikushev@users.noreply.github.com>
2025-10-13 18:20:35 +0300

    Updated auth middleware and route

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,22 @@
+import { Context, Next } from "koa";
+import passport from "../config/authConfig";
+
+export const authMiddleware = async (ctx: Context, next: Next) => {
+    return new Promise<void>((resolve, reject) => {
+        passport.authenticate('jwt', { session: false }, (err, user, info) => {
+            if (err) {
+                ctx.status = 500;
+                ctx.body = { error: "Internal Server Error" }
+                return reject(err);
+            }
+
+            if (!user) {
+                ctx.status = 401;
+                ctx.body = { error: 'Unauthorized: Invalid or missing user' }
+                return resolve();
+            }
+            ctx.state.user = {id: user.id, username: user.username}; // TODO: Change this from the signing payload on the jwt
+            resolve(next());
+        })(ctx, next)
+    })
+}
--- src/routes/auth/authRouter.ts
+++ src/routes/auth/authRouter.ts
@@ -1,12 +1,13 @@
 import Router from "@koa/router";
 import { data } from "../../../data/users";
 import { loginUser } from "../../services/authService";
+import { authMiddleware } from "../../middlewares/authMiddleware";
 
 export const authRouter = new Router({
     prefix: '/auth'
 });
 
-authRouter.get('/users', (ctx) => {
+authRouter.get('/users', authMiddleware, (ctx) => {
     ctx.body = data;
 })
 
