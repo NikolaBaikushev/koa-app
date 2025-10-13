f65b37ea69900bf84e47650f8f4cbdf22d312dc6
NikolaBaikushev <234618369+NikolaBaikushev@users.noreply.github.com>
2025-10-13 16:06:25 +0300

    Initialized project with Koa and Typescript

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+
+import Koa from 'koa';
+import Router from '@koa/router';
+import { config } from '../config';
+import json from 'koa-json'
+
+const app = new Koa();
+const router = new Router();
+
+app.use(json())
+app.use(router.routes()).use(router.allowedMethods());
+
+
+router.get('/', async (ctx) => {
+    ctx.body = {
+        "msg": "Hello"
+    }
+})
+
+
+app.listen(config.port, () => {
+    console.log('Server is running! ')
+})
