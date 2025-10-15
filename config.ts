2af86f3ab90df6dacd41b1ddf82ff86f47b06dcc
NikolaBaikushev <234618369+NikolaBaikushev@users.noreply.github.com>
2025-10-15 09:25:16 +0300

    Added env for secret

--- config.ts
+++ config.ts
@@ -5,5 +5,5 @@ dotenv.config();
 
 export const config = {
     PORT: process.env.PORT || 3000,
-    SECRET: 'asdsadmaskdma12321k3m12k3maskf13k4n123kn4'
+    SECRET: process.env.SECRET || 'asdsadmaskdma12321k3m12k3maskf13k4n123kn4'
 };
